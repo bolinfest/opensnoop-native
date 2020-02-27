7241ec6c4b02809f825a379cac5817d25909d342
rtkaratekid <mayfieldtristan@gmail.com>
2020-02-27 16:47:50 +0000

    fixed the build script to match the compiled libbpf name that BCC now uses: libbcc_bpf.so

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 # Note the generated opensnoop executable must be run with sudo.
 set -e
 python opensnoop.py
-clang opensnoop.c -lelf -O3 -o opensnoop /usr/lib/x86_64-linux-gnu/libbpf.so
+clang opensnoop.c -lelf -O3 -o opensnoop /usr/lib/x86_64-linux-gnu/libbcc_bpf.so
